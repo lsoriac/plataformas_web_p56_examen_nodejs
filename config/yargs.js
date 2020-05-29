eaaf1320da09eb5171f9eb9b4bb6178b628f0ab6
Santiago Soria <lsoriac@est.ups.edu.ec>
2020-05-29 09:34:17 -0500

    configuración yargs

---
+++ config/yargs.js
@@ -0,0 +1,41 @@
+const mostrar = {
+    archivo: {
+        demand: true,
+        alias: 'f',
+        desc: 'Permite establecer el path del archivo CSV que contiene los datos a analizar'
+    },
+    pais: {
+        alias: 'c',
+        default: 'ECU',
+        desc: 'Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3.'
+    },
+    anio: {
+        alias: 'y',
+        default: 1960,
+        desc: 'Permite especificar el año para el cual se requiere las estadísticas. Por defecto, 2018.'
+    }
+
+}
+const guardar = {
+    archivo: {
+        demand: true,
+        alias: 'f',
+        desc: 'Permite establecer el path del archivo CSV que contiene los datos a analizar'
+    },
+    pais: {
+        alias: 'c',
+        default: 'ECU',
+        desc: 'Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3.'
+    },
+    anio: {
+        alias: 'y',
+        default: 1960,
+        desc: 'Permite especificar el año para el cual se requiere las estadísticas. Por defecto, 2018.'
+    }
+}
+
+const argv = require('yargs').command('mostrar', ' Este comando publicará las estadísticas en una página web básica.', mostrar).command('guardar', ' Este comando almacenará los resultados de las estadísticas en un archivo json.', guardar).argv;
+
+module.exports = {
+    argv
+}
