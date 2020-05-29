e039cd602182af1a27b89f290535597fc1a69771
Santiago Soria <lsoriac@est.ups.edu.ec>
2020-05-29 11:09:05 -0500

    diseño básico de impresión

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-const argv = require('./config/yargs').argv
-    //const busca = require('./buscador/buscar')
-
-let comando = argv._[0];
+//Importación de módulos
+const argv = require('./Config/yargs').argv;
+const { analizar } = require('./buscador/buscar')
+let comando = argv._[0]
 let path = argv.archivo
 let pais = argv.pais
 let year = argv.anio
-
-switch (comando) {
-    case 'mostrar':
-        console.log("=============mostrar==========");
-        console.log(path);
-        break;
-    case 'guardar':
-        console.log("=============guardar==========");
-        console.log(pais);
-        break;
-    default:
-        'comando no reconocido'
+let data = ""
+let procesar = (callback) => {
+    if (comando != 'mostrar' && comando != 'guardar') {
+        console.log("Comando no reconocido");
+    } else {
+        analizar(pais, year, path)
+            .then(datos => {
+                data = datos
+                console.log(datos);
+                callback();
+            })
+            .catch(err => console.log(err.message))
+            //poneeeeer colllooooor rojjooooo
+    }
 }
+
+function switchF() {
+
+    switch (comando) {
+        case 'mostrar':
+            console.log("MOstrar");
+            console.log(year);
+            break;
+        case 'guardar':
+            console.log("Guardarr----");
+            console.log(year);
+
+            break;
+
+        default:
+            console.log("Comando no reconocido");
+    }
+}
+
+procesar(switchF)
